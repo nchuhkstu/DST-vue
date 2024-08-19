c39607607d475b2145ca836a9d02d39437fd5269
qhl666 <2789314645@qq.com>
2024-08-19 21:39:17 +0800

    同步

--- src/components/modSet.vue
+++ src/components/modSet.vue
@@ -1,22 +1,67 @@
 <template>
-
+    <div class="modSet">
+        <div class="list">
+            <div class="list-item" :id="`list-item-${index}`" v-for="(cluster,index) in clusterStore.clusters"@click=changeIndex(index)>{{ cluster.cluster_name }}</div>
+        </div>
+    </div>
 </template>
 <script>
+import {useClusterStore} from '../store/clusterStore'
 export default{
     name:'modSet',
     data(){
         return{
-
+            clusterStore:useClusterStore(),
         }
     },
     methods:{
-
+        changeIndex(index){
+            this.clusterStore.setIndex(Number(index));
+        },
     },
     mounted(){
         
+    },
+    activated(){
+        if(!this.clusterStore.clusters[this.clusterStore.index])
+            return;
+        this.clusterStore.refreshIndex();
     }
 }
 </script>
-<style>
-
+<style scoped>
+.modSet{
+    height: 100%;
+    width: 100%;
+    position: relative;
+}
+.list{
+    position: absolute;
+    width: calc(13% - 1.2vh);
+    height: calc(90% + 5.5vh - 1.2vh);
+    left: 1%;
+    top: 0vh;
+    background-color: rgba(110, 81, 47, 0.6);
+    border: 0.6vh solid rgb(118,82,44);
+    overflow: auto;
+}
+.list-item{
+    width: 90%;
+    height: 10%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 3vh;
+    margin: 1vh 5%;
+    cursor: pointer;
+    border-radius: 1vh;
+}
+.list-item.selected{
+    background-color: rgb(110,81,47);
+    color: rgb(224,173,71);
+}
+.list-item:hover{
+    background-color: rgb(110,81,47);
+    color: rgb(224,173,71);
+}
 </style>
