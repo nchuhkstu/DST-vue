e1eca3bb6af940c4de41c513cf61e2dbbd62d7db
qhl666 <2789314645@qq.com>
2024-09-13 01:02:54 +0800

    update

--- src/api/modRequest.js
+++ src/api/modRequest.js
@@ -1,7 +1,20 @@
 import { request } from "../utils/request";
 export function get_mods(page_size,current_page){
     return request({
-        url:`/${page_size}/${current_page}`,
+        url:`/mod/${page_size}/${current_page}`,
         method: 'get'
+    })
+}
+export function get(){
+    return request({
+        url:'/mod',
+        method: 'get'
+    })
+}
+export function focus_mod(data){
+    return request({
+        url:'/mod',
+        method: 'post',
+        data:data,
     })
 }
--- src/components/modSet.vue
+++ src/components/modSet.vue
@@ -4,16 +4,38 @@
             <div class="list-item" :id="`list-item-${index}`" v-for="(cluster,index) in clusterStore.clusters"@click=changeIndex(index)>{{ cluster.cluster_name }}</div>
         </div>
         <div class="set-container">
-            <div class="set-top">
-                <div class="navigation-button">模组设置</div>
-                <div class="navigation-button">模组订阅</div>
+            <div class="navigation">
+                <div class="navigation-button" @click="changeActive('set')">模组设置</div>
+                <div class="navigation-button" @click="changeActive('focus')">模组订阅</div>
             </div>
-            <div class="set-body">
-                <div class="mod" v-for="mod in mods">
-                    <div class="img"><img :src="mod.img"></div>
-                    <div class="mod-title">{{mod.title}}</div>
-                    <div class="mod-author">{{'作者：' + mod.author}}</div>
+            <div class="set-body" v-show="active=='set'">
+                <div class="set-left">
+                    <div class="set-left-top"></div>
+                    <div class="set-left-body">
+                        <div class="left-mod" v-for="mod in mods_focus">
+                            <div class="left-mod-img-container"><img class="left-mod-img" :src="mod.img"></div>
+                            <div class="left-mod-message">
+                                <div class="left-mod-message-top">{{ mod.title }}</div>
+                                <div class="left-mod-message-body"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <div class="set-right">
+                    <div class="set-top"></div>
+                    <div class="set-bottom"></div>
+                </div>
+            </div>
+            <div class="focus-body" v-show="active=='focus'">
+                <div class="mod-container">
+                    <div class="mod" v-for="mod in mods[currentPage]">
+                        <div class="img" @click="information(mod.href)"><img :src="mod.img"></div>
+                        <div class="mod-title">{{mod.title}}</div>
+                        <div class="mod-author">{{'作者：' + mod.author}}</div>
+                        <div class="focus" :id="mod.mod_id" @click="focusMod(mod)"><label v-show="mod.focus">已</label>订阅</div>
+                    </div>
+                </div>
+                
                 <div class="pages">
                     <div class="page-button" @click="Page1">首页</div>
                     <div class="page-button" @click="lastPage">上一页</div>
@@ -26,7 +48,8 @@
 </template>
 <script>
 import {useClusterStore} from '../store/clusterStore'
-import { get_mods } from '../api/modRequest';
+import { useTipStore } from '../store/tipStore';
+import { get_mods,focus_mod,get } from '../api/modRequest';
 export default{
     name:'modSet',
     data(){
@@ -34,7 +57,10 @@ export default{
             clusterStore:useClusterStore(),
             pageSize:18,
             currentPage:1,
-            mods:[],
+            mods:{},
+            mods_focus:[],
+            tipStore:useTipStore(),
+            active:'set',
         }
     },
     methods:{
@@ -44,9 +70,10 @@ export default{
         handleGetMods(pageSize,currentPage){
             return new Promise((resolve,reject)=>{
                 get_mods(pageSize,currentPage).then(response=>{
-                    this.mods = [];
+                    const array=[];
                     Object.keys(response.data).forEach(key=>{
-                        this.mods.push(response.data[key])
+                        array.push(response.data[key])
+                        this.mods[currentPage]=array;
                     })
                     console.log(this.mods);
                     resolve();
@@ -54,24 +81,57 @@ export default{
             })
 
         },
+        changeActive(active){
+            this.active = active;
+        },
+        handleGet(){
+            get().then(response=>{
+                console.log(response.data)
+                for(let i=0;i<response.data.length;i++){
+                    this.mods_focus.push(response.data[i]);
+                }
+                console.log(this.mods_focus);
+            })
+        },
         nextPage(){
-            this.handleGetMods(this.pageSize,this.currentPage+1).then(response=>{
-                this.currentPage++;
-            });
+            let num = this.currentPage + 1;
+            if(!(num in this.mods)){
+                this.handleGetMods(this.pageSize,this.currentPage+1)
+            }
+            this.currentPage++;
         },
         lastPage(){
-            this.handleGetMods(this.pageSize,this.currentPage-1).then(response=>{
-                this.currentPage--;
-            });
+            if(this.currentPage==1){
+                return
+            }
+            let num = this.currentPage - 1;
+            if(!(num in this.mods)){
+                this.handleGetMods(this.pageSize,this.currentPage-1)
+            }
+            this.currentPage--;
         },
         Page1(){
-            this.handleGetMods(this.pageSize,1).then(response=>{
-                this.currentPage=1;
-            });
+            this.currentPage = 1;
+        },
+        information(url){
+            window.open(url,'_blank');
+        },
+        focusMod(mod){
+            if(mod.focus){
+                this.tipStore.showTip("已经订阅过了");
+                return;
+            }
+            focus_mod(mod).then(response=>{
+                if(response.data.status=="ok"){
+                    document.getElementById(mod.mod_id).textContent = "已订阅"
+                }
+                this.tipStore.showTip(response.data.message);
+            })
         }
     },
     mounted(){
-        this.handleGetMods();
+        this.handleGet();
+        this.handleGetMods(this.pageSize, this.currentPage);
     },
     activated(){
         if(!this.clusterStore.clusters[this.clusterStore.index])
@@ -120,7 +180,7 @@ export default{
     height: calc(92% + 5.5vh);
     margin-left: 15%;
 }
-.set-top{
+.navigation{
     width: 100%;
     height: 5%;
     display: flex;
@@ -132,17 +192,97 @@ export default{
     width: calc(100% - 1.2vh);
     height: calc(95% - 1.2vh);
     border: 0.6vh solid rgb(118,82,44);
+    position: relative;
+    display: flex;
+}
+.set-left{
+    height: 100%;
+    width: calc(42% - 0.3vh);
+    border-right: 0.3vh solid orange;
+    overflow: auto
+}
+.set-left-top{
+    width: 100%;
+    height: 10%;
+}
+.set-left-body{
+    width: 100%;
+    height: 90%;
+}
+.left-mod{
+    border: 0.3vh solid black;
+    /* background-color: rgb(98, 83, 66); */
+    height: 20%;
+    width: calc(100% - 6% - 0.6vh);
+    display: flex;
+    margin-bottom: 2%;
+    border-radius: 1vh;
+    margin-left: 3%;
+    margin-right: 3%;
+}
+.left-mod-img-container{
+    height: 100%;
+    width: 30%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.left-mod-img{
+    height: 95%;
+    width: 95%;
+}
+.left-mod-message{
+    height: 100%;
+    width: 68%;
+    margin-left: 2%;
+}
+.left-mod-message-top{
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    width: 100%;
+    height: 40%;
+    font-size: 2.5vh;
+}
+.left-mod-message-body{
+    width: 100%;
+    height: 60%;
+}
+.set-right{
+    height: 100%;
+    width: 58%;
+}
+.set-top{
+    height: calc(22% - 0.3vh);
+    width: 100%;
+    border-bottom: 0.3vh solid orange;
+}
+.set-bottom{
+    height: 78%;
+    width: 100%;
+}
+.focus-body{
+    width: calc(100% - 1.2vh);
+    height: calc(95% - 1.2vh);
+    border: 0.6vh solid rgb(118,82,44);
+    position: relative;
+}
+.mod-container{
+    height: 95%;
+    width: 100%;
     display: flex;
     flex-wrap: wrap;
     gap: 0 1vw;
 }
 .mod{
-    height: 30%;
-    width: calc(16.6% - 0.835vw);
+    padding: 0 0.1vw;
+    height: 33.3%;
+    width: calc(16.6% - 0.835vw - 0.2vw);
 }
 .img{
     width: 100%;
-    height: 80%;
+    height: 65%;
+    cursor: pointer;
 }
 img{
     height: 100%;
@@ -154,12 +294,29 @@ img{
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
+    color: white;
 }
 .mod-author{
     height: 10%;
+    color: rgb(201, 191, 191);
     font-size: 1.5vh;
 }
+.focus{
+    font-size: 2vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 1vh;
+    height: calc(10% - 0.6vh);
+    width: calc(100% - 0.6vh);
+    border: 0.3vh solid rgb(118,82,44);
+    background-color: rgb(110,81,47);
+    cursor: pointer;
+}
 .pages{
+    position: absolute;
+    bottom: 0;
+    left: 0;
     height: 5%;
     width: 100%;
     display: flex;
